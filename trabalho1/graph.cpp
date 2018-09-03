05dc956fb5a4b8396977c8f6bc62e47e2174a68d
luthierycosta <luthierycosta2010@gmail.com>
2018-09-02 23:33:59 -0300

    Começando

---
+++ trabalho1/graph.cpp
@@ -0,0 +1,64 @@
+#include <iostream>		// cin e cout
+#include <algorithm>	// max
+#include <list>
+#include <iterator>
+#include <utility>		// pair
+
+using namespace std;
+
+class Graph {
+	list<Vertex> vertList;
+	list<Edge> edgeList;
+
+public:
+	void addVert(string label) {
+		vertList.push_back(new Vertex(label));
+	}
+
+	void addEdge(Vertex a, Vertex b, int value) {
+		edgeList.push_back(new Edge(a, b, value));
+	}
+
+	int degree() {
+		int res = 0;
+		for (Vertex v: vertList)
+			res = max(res, v.degree());
+
+		return res;
+	}
+}
+
+class Vertex {
+	string label;
+	list<Vertex> adj_list;
+
+public:
+	Vertex(string str) {
+		this.label = str;
+	}
+
+	void addAdj(Vertex v) {
+		adj_list.push_back(v);
+	}
+
+	int degree() {
+		return adj_list.size;
+	}
+}
+
+class Edge {
+	int value;
+	pair<Vertex,Vertex> vertices;
+
+public:
+	Edge(Vertex a, Vertex b, int value) {
+		this.value = value;
+		vertices = make_pair(a, b);
+		a.addAdj(b);
+		b.addAdj(a);
+	}
+
+	bool isLoop() {
+		return vertices.first == vertices.second;
+	}
+}
