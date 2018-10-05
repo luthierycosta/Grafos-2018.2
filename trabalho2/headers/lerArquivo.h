a7c4fb74b305f45cefc6deed15dea6d9f9dcd517
KomodoDile <lucasvini.98@hotmail.com>
2018-10-05 19:46:24 -0300

    Add files via upload

---
+++ trabalho2/headers/lerArquivo.h
@@ -0,0 +1,12 @@
+#ifndef _READFILE_
+#define _READFILE_
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void lerArquivo(Grafo& grafo, string nome_arquivo);
+
+#endif
